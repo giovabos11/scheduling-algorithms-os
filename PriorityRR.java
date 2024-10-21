783e5e5ad2a3b743b219c00cc612312106806035
giovabos11 <giovabos11@gmail.com>
2024-10-21 15:52:08 -0500

    Priority Round Robin algorithm implemented

--- PriorityRR.java
+++ PriorityRR.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class PriorityRR implements Algorithm {
@@ -17,41 +18,55 @@ public class PriorityRR implements Algorithm {
     public void schedule() {
         System.out.println("Priority with RR Scheduling \n");
 
-        nextTaskIndex = 0;
-
         // While there are items in the ready queue
         while (!queue.isEmpty()) {
-            // Pick the next task to run
-            currentTask = pickNextTask();
+            // Pick the tasks with the highest priority
+            List<Task> currentTasks = pickNextTasks();
 
-            // If there are no other processes with the same priority
-            if (isHighestTaskPriority(currentTask)) {
+            // If there is only one task with the highest priority, run that task for the full time
+            if (currentTasks.size() == 1) {
+                // Pick the next task to run
+                currentTask = currentTasks.get(0);
+
                 // Run process for the entire time
                 CPU.run(currentTask, currentTask.getBurst());
 
                 // Update process burst time
                 currentTask.setBurst(0);
-            }
-            // Otherwise, round-robin
-            else {
-                // Run process for quantum time or remaining
-                CPU.run(currentTask, Math.min(quantum, currentTask.getBurst()));
 
-                // Update process burst time
-                currentTask.setBurst(currentTask.getBurst() - Math.min(quantum, currentTask.getBurst()));
-            }
-
-            // Check to remove the task from the queue
-            if (currentTask.getBurst() <= 0) {
-                System.out.println("Task " + currentTask.getName() + " Finished\n");
+                // Remove the task from the queues
+                System.out.println("Task " + currentTask.getName() + " finished.\n");
+                currentTasks.remove(currentTask);
                 queue.remove(currentTask);
-                nextTaskIndex--;
             }
+            // Otherwise, more than one task with the same priority, do round-robin
+            else {
+                int currentTasksIndex = 0;
+                // Iterate through the queue
+                while (!currentTasks.isEmpty()) {
+                    // Pick the next task to run
+                    currentTask = currentTasks.get(currentTasksIndex);
 
-            // Update next task index
-            nextTaskIndex++;
-            if (!queue.isEmpty()) {
-                nextTaskIndex = nextTaskIndex % queue.size();
+                    // Run process for quantum time or remaining
+                    CPU.run(currentTask, Math.min(quantum, currentTask.getBurst()));
+
+                    // Update process burst time
+                    currentTask.setBurst(currentTask.getBurst() - Math.min(quantum, currentTask.getBurst()));
+
+                    // Check to remove the task from the queues
+                    if (currentTask.getBurst() <= 0) {
+                        System.out.println("Task " + currentTask.getName() + " finished.\n");
+                        currentTasks.remove(currentTask);
+                        queue.remove(currentTask);
+                        currentTasksIndex--;
+                    }
+
+                    // Update the current tasks index
+                    currentTasksIndex++;
+                    if (!currentTasks.isEmpty()) {
+                        currentTasksIndex = currentTasksIndex % currentTasks.size();
+                    }
+                }
             }
         }
     }
@@ -71,7 +86,28 @@ public class PriorityRR implements Algorithm {
         return highestPriority;
     }
 
-    private boolean isHighestTaskPriority(Task task) {
-        return true;
+    /**
+     * This method gets a list of all the processes with the highest priority
+     */
+    public List<Task> pickNextTasks() {
+        // Create a temporary variable to hold the highest priority task
+        Task highestPriority = new Task("", Integer.MIN_VALUE, Integer.MIN_VALUE);
+        // Create a temporary tasks list
+        List<Task> tasks = new ArrayList<>();
+
+        // Look for the highest priority task in the queue (and add ties)
+        for (int i = 0; i < queue.size(); i++) {
+            if (queue.get(i).getPriority() > highestPriority.getPriority()) {
+                highestPriority = queue.get(i);
+                tasks.clear();
+                tasks.add(queue.get(i));
+            }
+            else if (queue.get(i).getPriority() == highestPriority.getPriority()) {
+                tasks.add(queue.get(i));
+            }
+        }
+
+        // Return the highest priority tasks (if they are tied)
+        return tasks;
     }
 }
