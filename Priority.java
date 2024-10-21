8c61877b4b9e91961e6a1f804ad588eea93a381c
giovabos11 <giovabos11@gmail.com>
2024-10-21 13:26:11 -0500

    Priority algorithm implemented

--- Priority.java
+++ Priority.java
@@ -1,14 +1,44 @@
 import java.util.List;
 
 public class Priority implements Algorithm {
-    public Priority(List<Task> queue) {
+    private List<Task> queue;
+    private Task currentTask;
 
+    private int tasksRun;
+
+    public Priority(List<Task> queue) {
+        this.queue = queue;
+        this.tasksRun = queue.size();
     }
 
     public void schedule() {
+        System.out.println("Priority Scheduling \n");
 
+        // While there are items in the ready queue
+        while (!queue.isEmpty()) {
+            // Pick the next task to run
+            currentTask = pickNextTask();
+
+            // Run process
+            CPU.run(currentTask, currentTask.getBurst());
+
+            // Remove the task from the queue
+            queue.remove(currentTask);
+        }
     }
+
     public Task pickNextTask() {
-        return null;
+        // Create a temporary variable to hold the highest priority task
+        Task highestPriority = new Task("", Integer.MIN_VALUE, Integer.MIN_VALUE);
+
+        // Look for the highest priority task in the queue
+        for (int i = 0; i < queue.size(); i++) {
+            if (queue.get(i).getPriority() > highestPriority.getPriority()) {
+                highestPriority = queue.get(i);
+            }
+        }
+
+        // Return the highest priority task
+        return highestPriority;
     }
 }
